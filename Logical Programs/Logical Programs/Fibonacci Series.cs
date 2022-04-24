86597117b9e349fc806c8a84c51088ef60272889
roshni1015 <roshniadatrao1110@gmail.com>
2022-04-24 17:50:01 +0530

    FibonacciSeries

---
+++ Logical Programs/Logical Programs/Fibonacci Series.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    internal class Fibonacci_Series
+    {
+        public void FibonacciSeries()
+        {
+            int n1 = 0, n2 = 1, n3, j, num;
+            Console.Write("Enter the number of elements: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " "); 
+            for (j = 2; j < num; ++j)     
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
--- Logical Programs/Logical Programs/Program.cs
+++ Logical Programs/Logical Programs/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Logical_Programs;
+
 Console.WriteLine("Welcome to logical Programs!");
+while (true)
+{
+
+    Console.WriteLine("\nChoose the Program to be executed :\n1)Fibonacci_Series");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            Fibonacci_Series febinocci = new();
+            febinocci.FibonacciSeries();
+            break;
+        default:
+            break;
+    }
+}
