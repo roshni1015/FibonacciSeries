0cbfdaf281a6d0a18a6a3d2418a6dbd06ed65891
roshni1015 <roshniadatrao1110@gmail.com>
2022-04-24 18:05:19 +0530

    PerfectNumber

---
+++ Logical Programs/Logical Programs/PerfectNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    internal class PerfectNumber
+    {
+        public void perfectnumber()
+        {
+            {
+                int num, sum = 0, n;
+                Console.Write("Enter the Number: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                n = num;
+                for (int i = 1; i < num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        sum = sum + i;
+                    }
+                }
+                if (sum == n)
+                {
+                    Console.WriteLine("\n Number is a Perfect number");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("\n Number is not a Perfect number");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
--- Logical Programs/Logical Programs/Program.cs
+++ Logical Programs/Logical Programs/Program.cs
@@ -5,13 +5,17 @@ Console.WriteLine("Welcome to logical Programs!");
 while (true)
 {
 
-    Console.WriteLine("\nChoose the Program to be executed :\n1)Fibonacci_Series");
+    Console.WriteLine("\nChoose the Program to be executed :\n1)Fibonacci_Series\n2)PerfectNumber");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
         case 1:
             Fibonacci_Series febinocci = new();
             febinocci.FibonacciSeries();
+            break;
+        case 2:
+            PerfectNumber perfect = new();
+            perfect.perfectnumber();
             break;
         default:
             break;
